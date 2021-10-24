fc985cbbae89024da59244d237dc3704c3667788
Kristian Ivanov <kriskopk@abv.bg>
2021-10-24 22:32:32 +0300

    finished stories going for posts
    finished stories going for posts

--- components/Feed.js
+++ components/Feed.js
@@ -1,22 +1,20 @@
+import Posts from "./Posts.js";
 import Stories from "./Stories.js";
 
 function Feed() {
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 
-    xl:max-w-6xl max-auto">
+    xl:max-w-6xl mx-auto">
       {/* section */}
       <section className="col-span-2">
-      {/* stories */}
         <Stories />
+        <Posts />
       </section>
-      {/* posts */}
 
-      {/* section 2 */}
       <section>
-
-      </section>
       {/* mini profile */}
       {/* suggestions */}
+      </section>
     </main>
   );
 }
---
+++ components/Posts.js
@@ -0,0 +1,11 @@
+import Post from "./Post.js"
+
+function Posts() {
+    return (
+        <div>
+            <Post />
+        </div>
+    )
+}
+
+export default Posts
