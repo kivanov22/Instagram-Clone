d5c8d3d4c2e7e1839241ccbbc6eac7e9287075a3
Kristian Ivanov <kriskopk@abv.bg>
2021-12-13 13:17:09 +0200

    updated

--- components/Post.js
+++ components/Post.js
@@ -113,13 +113,14 @@ function Post({ id, username, userImg, img, caption }) {
       )}
 
       {/* caption */}
-      <p className="p-5 truncate">
+      {/* fix this */}
+      <div className="p-5 truncate">
         {likes.length > 0 && (
           <p className="font-bold mb-1">{likes.length} likes</p>
         )}
         <span className="font-bold mr-1">{username} </span>
         {caption}
-      </p>
+      </div>
 
       {/* comments */}
       {comments.length > 0 && (
